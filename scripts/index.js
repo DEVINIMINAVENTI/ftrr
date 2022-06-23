d5cfafbc56e5660ac07004defba4bfe7546ad966
DEVINIMINAVENTI <96622416+DEVINIMINAVENTI@users.noreply.github.com>
2022-06-23 11:27:43 +0100

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -23,3 +23,43 @@ function navToggles(){
     nav.classList.toggle("showshops");
    
 }
+
+
+
+function showNotification(){
+
+    const notification = new Notification("Message from HANDOUTS STORE!", {
+
+        body: "We now have Handouts for Microbiology Second Semester",
+
+        icon: "https://www.handouts.store/images/header.jpeg"
+
+    });
+
+    notification.onclick = (e) => {
+
+        window.location.href = "https://www.handouts.store/handouts/100lvlsls/100LVLMCB/";
+
+    };
+
+}
+
+console.log(Notification.permission);
+
+if (Notification.permission === "granted"){
+
+    showNotification();
+
+}  else if (Notification.permission !== "denied"){
+
+    Notification.requestPermission().then(permission => {
+
+        if (permission === "granted"){
+
+            showNotification();
+
+        }
+
+    });
+
+}
