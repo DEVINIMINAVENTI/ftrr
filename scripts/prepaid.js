7654c7202a11ce2e87f3f28d5d972c779210fe2f
DEVINIMINAVENTI <96622416+DEVINIMINAVENTI@users.noreply.github.com>
2022-09-21 09:30:36 +0100

    Add files via upload

---
+++ scripts/prepaid.js
@@ -0,0 +1,10 @@
+window.addEventListener("load", function(){
+    setTimeout("location.href='https://www.handouts.store/'",10000);
+  })
+
+  
+  
+function bling(){
+    setTimeout("location.href='//www.handouts.store/'",1000);
+  }
+  
