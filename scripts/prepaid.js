31ccc842f1a4511bcc5a36af9c81b4988443ea51
DEVINIMINAVENTI <96622416+DEVINIMINAVENTI@users.noreply.github.com>
2022-10-14 13:34:49 +0100

    Update prepaid.js

--- scripts/prepaid.js
+++ scripts/prepaid.js
@@ -1,5 +1,5 @@
 window.addEventListener("load", function(){
-    setTimeout("location.href='https://www.handouts.store/'",5000);
+    setTimeout("location.href='https://www.handouts.store/handouts/100lvl/'",5000);
   })
 
   
